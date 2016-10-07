0d218a2ec6ebf45c6fa6172275caddf1e4099ebe
Ziesemann <michael.ziesemann@haw-hamburg.de>
2016-10-07 15:50:14 +0200

    The bird begins to move

---
+++ FlippyBird/Assets/Scripts/FlappyInput.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FlappyInput : MonoBehaviour {
+
+    public float jumpforce = 40f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        DetectInput();
+	}
+
+    void DetectInput()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Debug.Log("ButtonPressed");
+            Jump();
+        }
+    }
+
+    void Jump ()
+    {
+        gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0f,0f,0f);
+        gameObject.GetComponent<Rigidbody>().AddForce(new Vector3(0f, jumpforce, 0f));
+    }
+
+}
